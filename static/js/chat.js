c0e005afcf2568b147f1e73d4543c56e82bb3364
hwanghye00 <hwangshinhye.work@gmail.com>
2022-07-18 14:17:11 +0900

    fixed bug

--- static/js/chat.js
+++ static/js/chat.js
@@ -2,17 +2,14 @@ const asideChat = document.getElementById("chat-container");
 
 // Chat List Open
 function activeChatList() { 	
-    if(asideChat.style.display == 'none') {
-        asideChat.style.display = 'block'; 	
-        
-    } else {
-        asideChat.style.display = 'none'; 	
+    if(asideChat.style.display == '') {
+        asideChat.style.display = 'block'; 		
     } 
 }
 
-// Chat List Close
+// Chat List Close 
 function DisableChatList() { 
     if(asideChat.style.display == 'block') {
-        asideChat.style.display = 'none'
+        asideChat.style.display = ''
     }
 }
