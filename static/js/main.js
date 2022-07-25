fcbd9e645e8d8e0a90a79fcd9eba9307cfc2bc00
SeonminKim1 <yubi5050@naver.com>
2022-07-25 17:40:30 +0900

    Refactor Code and Add skills object by InsertProject #6

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,14 @@
 var login_username;
 var sidepro_skill_list = new Array(); // sidepro_skill_list
 var skill_tag_list = new Array(); // 검색 추가한 skills tag list
+var skills_object = {};
 
 const filter_div_tag = document.querySelector('.box-search-tag') // tag 추가될 div
 const filter_input_tag = document.querySelector(".input-search-main") // input 창
 const filter_datalist_tag = document.getElementById("search-list") // Datalist Tag 
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("chat.js - DOMContentLoaded")
+    console.log("main.js - DOMContentLoaded")
     GetBaseInfo()
     // set_filtering_initalize()
 });
@@ -37,6 +38,7 @@ async function  GetBaseInfo(){
 // 필터링 Skill들(option) 목록(datalist)에 추가 
 function SetSkillsFilteringInitalize(skills){
     for(let i=0; i<skills.length; i++){
+        skills_id = skills[i]['id']
         skill_name = skills[i]['name']
         // skill option 만들기
         var SkillOptionTag = document.createElement('option')
@@ -46,6 +48,9 @@ function SetSkillsFilteringInitalize(skills){
 
         // sidepro 에서 제공하는 skill list
         sidepro_skill_list.push(skill_name)
+        
+        // skills => object initialize
+        skills_object[skill_name] = skills_id
     }
 }
 
@@ -80,100 +85,3 @@ function DrawSkillTag(){
         }
     }
 }
-
-async function getFilterResult() {
-    console.log("main.js - getFilterResult")
-    const ordering_radio = document.getElementsByName('ordering')
-    const price_radio = document.getElementsByName('price')
-    const img_shape_radio = document.getElementsByName('img_shape')
-
-    let category_name;
-    if(localStorage.getItem('category_name')){
-        category_name = localStorage.getItem('category_name')
-    }else{
-        category_name = `${DEFAULT_CATEGORY}`
-    }
-
-    var ordering_value='', price_value='', img_shape_value='';
-    ordering_radio.forEach((node) => { if(node.checked){ ordering_value = node.value }}) 
-    price_radio.forEach((node) => { if(node.checked){ price_value = node.value }})
-    img_shape_radio.forEach((node) => { if(node.checked){ img_shape_value = node.value }})
-
-    console.log(ordering_value + '/' + price_value + '/' + img_shape_value);
-
-    const response = await fetch(
-        `${backend_base_url}/product/?category_name=${category_name}&price=${price_value}&image_shape=${img_shape_value}&ordering_value=${ordering_value}`,
-        {
-        headers:{
-            Accept: "application/json",
-            'content-type': "application/json"
-        },
-        method: 'GET',
-        // body: JSON.stringify(Data)
-    })
-    console.log('============================================', response)
-    
-    response_json = await response.json()
-    
-    if(response.status == 200) {
-        MainProductPutData(response_json)
-    } else {
-        alert('ERROR: ', response.status)
-    }
-}
-
-// 필터링
-async function getSearchResult() {
-    console.log("main.js - getSearchResult")
-    search_input = document.querySelector(".item-search")
-    search_input_value = search_input.value
-    console.log(search_input_value)  
-
-    let category_name;
-    if(localStorage.getItem('category_name')){
-        category_name = localStorage.getItem('category_name')
-    }else{
-        category_name = `${DEFAULT_CATEGORY}`
-    }
-
-    const response = await fetch(
-        `${backend_base_url}/product/${category_name}/${search_input_value}`,
-        {
-        headers:{
-            Accept: "application/json",
-            'content-type': "application/json"
-        },
-        method: 'GET',
-        // body: JSON.stringify(Data)
-    })
-    console.log('============================================', response)
-    
-    response_json = await response.json()
-    
-    if(response.status == 200) {
-        MainProductPutData(response_json)
-    } else {
-        alert('ERROR: ', response.status)
-    }
-}
-
-// 필터링
-async function getFilterInitialize() {
-    console.log("main.js - getFilterInitialize")
-    const ordering_radio = document.getElementsByName('ordering')
-    const price_radio = document.getElementsByName('price')
-    const img_shape_radio = document.getElementsByName('img_shape')
-
-    ordering_radio.forEach((node) => { if(node.checked){ 
-        node.checked=false;
-
-    }}) 
-    price_radio.forEach((node) => { if(node.checked){ 
-        node.checked=false 
-    }})
-    img_shape_radio.forEach((node) => { if(node.checked){
-        node.checked=false 
-    }})
-
-}
-
