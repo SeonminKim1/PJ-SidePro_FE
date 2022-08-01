71a777f0aeaf07b70e0a2c304edc8e726dcaedb3
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-08-02 00:30:14 +0900

    Hotfix - _base.js

--- static/js/_base.js
+++ static/js/_base.js
@@ -1,15 +1,15 @@
 // For All
 var is_local = true
-    if(is_local == true){
-        const backend_base_url = "http://127.0.0.1:8000"
-        const backend_base_ip_port = "127.0.0.1:8000"      
-        const frontend_base_url = "http://127.0.0.1:5500"
-    }else{
-        const backend_base_url = "http://3.37.194.222:8000"
-        const backend_base_ip_port = "3.37.194.222:8000"      
-        // const frontend_base_url = "http://www.sidepro.shop"
-        const frontend_base_url = "http://sidepro.shop.s3-website.ap-northeast-2.amazonaws.com"
-    }
+if (is_local == true) {
+    const backend_base_url = "http://127.0.0.1:8000"
+    const backend_base_ip_port = "127.0.0.1:8000"
+    const frontend_base_url = "http://127.0.0.1:5500"
+} else {
+    const backend_base_url = "http://3.37.194.222:8000"
+    const backend_base_ip_port = "3.37.194.222:8000"
+    // const frontend_base_url = "http://www.sidepro.shop"
+    const frontend_base_url = "http://sidepro.shop.s3-website.ap-northeast-2.amazonaws.com"
+}
 
 const ROOM_STATUS_RUNNING = 'running'
 const ROOM_STATUS_PENDING = 'pending'
@@ -27,26 +27,5 @@ function StringToDatetime(string_time, type) { // StringToDatetime("2022-07-25T1
     }
 }
 
-const PROJECT_BASE_IMAGE = "https://toastuitestbucket.s3.ap-northeast-2.amazonaws.com/project-imgs/project_thumnail.jpeg"
-const PROFILE_BASE_IMAGE = "https://toastuitestbucket.s3.ap-northeast-2.amazonaws.com/profile-park_2022-07-25_14%3A57%3A33"
-
-// function StringToDatetime(string_time, type){
-//     var chat_time, dt;
-//     if(type == 1){
-//         chat_time = new Date(string_time);
-//         dt = chat_time.getFullYear()+'-'+(chat_time.getMonth()+1)+'-'+chat_time.getDate() + ' ' +
-//              chat_time.getHours() + ":" + chat_time.getMinutes() + ":" + chat_time.getSeconds();
-//     }else if(type == 2){ // 2022-07-15
-//         chat_time = new Date(string_time);
-//         dt = chat_time.getFullYear()+'-'+(chat_time.getMonth()+1)+'-'+chat_time.getDate()
-//     }
-//     return timestamp(dt)
-// }
-
-
-// Get UUID v4
-// function uuidv4() {
-//     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-//       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-//     );
-// }  
+const PROFILE_BASE_IMAGE = "https://s3.ap-northeast-2.amazonaws.com/sidepro.shop/static/img/profile_base.png"
+const PROJECT_BASE_IMAGE = "https://s3.ap-northeast-2.amazonaws.com/sidepro.shop/static/img/project_thumnail.jpeg"
